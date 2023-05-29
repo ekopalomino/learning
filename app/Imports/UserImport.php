a583d84073631b65555a3f78307ad4951bc09aca
Eko Heru Wibowo <heru.palomino@gmail.com>
2023-05-29 10:16:16 +0700

    update

--- app/Imports/UserImport.php
+++ app/Imports/UserImport.php
@@ -6,8 +6,9 @@ use iteos\Models\User;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
 
-class UserImport implements ToModel, WithHeadingRow
+class UserImport implements ToModel, WithHeadingRow, WithBatchInserts
 {
     /**
     * @param array $row
@@ -23,5 +24,10 @@ class UserImport implements ToModel, WithHeadingRow
             'division_id' => $row['divisi'],
             'department_id' => $row['departemen'],
         ]);
+    }
+
+    public function batchSize(): int
+    {
+        return 100;
     }
 }
